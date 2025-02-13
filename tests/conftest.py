7ea4c87de9384aa30c57db88c6b58d0a46502caa
Pifchick <aep@level.travel>
2025-02-13 12:27:14 +0300

    Add file for lesson 5. Add --headless for fix work time

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
+from pickle import FALSE
+
 import pytest
 from selene import browser
+from selenium import webdriver
 
 #For all test and browesrs
 @pytest.fixture(scope= "function", autouse=True)
@@ -14,6 +17,9 @@ def main_fixture(browser_timeout, browser_open_and_quit):
 @pytest.fixture(scope="function", autouse=False)
 def browser_open_and_quit():
     browser.open('https://school.qa.guru/cms/system/login')
+    driver_options = webdriver.ChromeOptions() # Задаем в каком бразере
+    driver_options.add_argument('--headless') # Выключает отображение браузера
+    browser.config.driver_options = driver_options # Передаем конфигарцию в config.driver_options
     yield
     #browser.driver.delete_all_cookies() - надо доделать
     browser.quit()
@@ -21,7 +27,12 @@ def browser_open_and_quit():
 #For yandex browser
 @pytest.fixture(scope= "function", autouse=False)
 def browser_yandex_open_and_quit():
+    # browser.config.driver_name = 'chrome' #Или так
+    driver_options = webdriver.FirefoxOptions() # Задаем в каком бразере, надо разобрать как яндекс добавить
+    driver_options.add_argument('--headless') # Выключает отображение браузера
+    browser.config.driver_options = driver_options
     browser.open('https://ya.ru/')
+
     yield
     #browser.driver.delete_all_cookies() - надо доделать
     browser.quit()
